bb56501e1ec976761faadb6088f1f241db3ffd71
i2002215 <2002215@sit.singaporetech.edu.sg>
2022-04-15 08:31:58 +0000

    third commit

--- multiple_calls.py
+++ multiple_calls.py
@@ -1,9 +1,12 @@
-# writing a simple file
+# writing a new function
 
 def func_a():
   print("hello world again!")
 
 def func_b():
   print("this is a second function")
+
+def func_c():
+  print("this is a third function")
 
 func_a()
