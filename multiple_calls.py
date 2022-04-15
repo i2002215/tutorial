60296eeb8ddac4900a0f5dd779a64a8d2a215cd0
i2002215 <2002215@sit.singaporetech.edu.sg>
2022-04-15 08:37:31 +0000

    accidentally erased info

--- multiple_calls.py
+++ multiple_calls.py
@@ -1,12 +1,4 @@
-# writing a new function
 
 def func_a():
-  print("hello world again!")
-
-def func_b():
-  print("this is a second function")
-
-def func_c():
-  print("this is a third function")
 
 func_a()
